75dde4819ed68342489a5b884a544f7867956682
mandriota <mandriotamark@gmail.com>
2024-11-20 13:49:40 +0100

    feat: download small files; extract handler

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log/slog"
+	"net/http"
 	"os"
 	"os/signal"
 	"strings"
@@ -22,23 +24,32 @@ import (
 
 type Config map[string][]string
 
-const ForwarderHookName = "ForwarderHook"
+const ForwarderHookName = "ForwarderHook2"
 
-func loadConfig(filePath string) (Config, error) {
+func loadConfig(cfg *Config, filePath string) error {
 	file, err := os.Open(filePath)
 	if err != nil {
-		return nil, err
+		return err
 	}
 	defer file.Close()
 
-	config := Config{}
-	if err := json.NewDecoder(file).Decode(&config); err != nil {
-		return nil, err
+	if err := json.NewDecoder(file).Decode(cfg); err != nil {
+		return err
 	}
-	return config, nil
+	return nil
 }
 
-func getOrCreateWebhook(client bot.Client, channelID snowflake.ID) (*discord.IncomingWebhook, error) {
+type Stats struct {
+	MessageForwardCount uint64
+}
+
+type Handler struct {
+	ctx   context.Context
+	cfg   Config
+	stats Stats
+}
+
+func (h *Handler) getOrCreateWebhook(client bot.Client, channelID snowflake.ID) (*discord.IncomingWebhook, error) {
 	webhooks, err := client.Rest().GetWebhooks(channelID)
 	if err != nil {
 		return nil, err
@@ -55,8 +66,77 @@ func getOrCreateWebhook(client bot.Client, channelID snowflake.ID) (*discord.Inc
 	})
 }
 
+func (h *Handler) onMessageCreate(e *events.MessageCreate) {
+	if e.Message.Author.Bot {
+		return
+	}
+
+	targetChannels, ok := h.cfg[e.ChannelID.String()]
+	if !ok {
+		return
+	}
+
+	fmt.Print("\nforwarded messages count: ", atomic.AddUint64(&h.stats.MessageForwardCount, 1))
+
+	for _, targetChannelID := range targetChannels {
+		targetID, err := snowflake.Parse(targetChannelID)
+		if err != nil {
+			e.Client().Logger().Error("failed to parse channel ID", "error", err)
+			continue
+		}
+
+		forwarderWebhook, err := h.getOrCreateWebhook(e.Client(), targetID)
+		if err != nil {
+			e.Client().Logger().Error("failed to get/create webhook", "error", err)
+			continue
+		}
+
+		content := strings.Builder{}
+		content.WriteString(e.Message.Content)
+
+		messageBuilder := discord.NewWebhookMessageCreateBuilder().
+			SetAvatarURL(*e.Message.Author.AvatarURL()).
+			SetUsername(e.Message.Author.Username)
+
+		bodies := []io.ReadCloser{}
+
+		for _, attachment := range e.Message.Attachments {
+			if attachment.Size <= 1<<15 {
+				desc := ""
+				if attachment.Description != nil {
+					desc = *attachment.Description
+				}
+				resp, err := http.Get(attachment.URL)
+				if err != nil {
+					e.Client().Logger().Error("failed to download attachment", "error", err)
+					continue
+				}
+				bodies = append(bodies, resp.Body)
+				messageBuilder.AddFile(attachment.Filename, desc, resp.Body)
+				continue
+			}
+
+			content.WriteByte('\n')
+			content.WriteString(attachment.URL)
+		}
+
+		messageBuilder.SetContent(content.String())
+
+		forwarderClient := webhook.New(forwarderWebhook.ID(), forwarderWebhook.Token)
+		if _, err := forwarderClient.CreateMessage(messageBuilder.Build()); err != nil {
+			e.Client().Logger().Error("failed to send message via webhook", "error", err)
+		}
+		forwarderClient.Close(h.ctx)
+
+		for _, body := range bodies {
+			body.Close()
+		}
+	}
+}
+
 func main() {
 	ctx := context.Background()
+	handler := Handler{ctx: ctx}
 
 	cfgPath := "config.json"
 	if len(os.Args) >= 2 {
@@ -66,14 +146,11 @@ func main() {
 	}
 
 	slog.Info("reading config...")
-	
-	cfg, err := loadConfig(cfgPath)
-	if err != nil {
+
+	if err := loadConfig(&handler.cfg, cfgPath); err != nil {
 		slog.Error("failed to load config", "error", err)
 		return
 	}
-
-	forwardCounter := int64(0)
 
 	client, err := disgo.New(os.Getenv("BRIDGE_BOT_TOKEN"),
 		bot.WithGatewayConfigOpts(
@@ -83,51 +160,7 @@ func main() {
 				gateway.IntentMessageContent,
 			),
 		),
-		bot.WithEventListenerFunc(func(e *events.MessageCreate) {
-			if e.Message.Author.Bot {
-				return
-			}
-
-			targetChannels, ok := cfg[e.ChannelID.String()]
-			if !ok {
-				return
-			}
-
-			fmt.Print("\nforwarded messages count: ", atomic.AddInt64(&forwardCounter, 1))
-			
-			for _, targetChannelID := range targetChannels {
-				targetID, err := snowflake.Parse(targetChannelID)
-				if err != nil {
-					e.Client().Logger().Error("failed to parse channel ID", "error", err)
-					continue
-				}
-
-				forwarderWebhook, err := getOrCreateWebhook(e.Client(), targetID)
-				if err != nil {
-					e.Client().Logger().Error("failed to get/create webhook", "error", err)
-					continue
-				}
-
-				content := strings.Builder{}
-				content.WriteString(e.Message.Content)
-
-				for _, attachment := range e.Message.Attachments {
-					content.WriteByte('\n')
-					content.WriteString(attachment.URL)
-				}
-
-				messageBuilder := discord.NewWebhookMessageCreateBuilder().
-					SetAvatarURL(*e.Message.Author.AvatarURL()).
-					SetUsername(e.Message.Author.Username).
-					SetContent(content.String())
-
-				forwarderClient := webhook.New(forwarderWebhook.ID(), forwarderWebhook.Token)
-				if _, err := forwarderClient.CreateMessage(messageBuilder.Build()); err != nil {
-					e.Client().Logger().Error("failed to send message via webhook", "error", err)
-				}
-				forwarderClient.Close(ctx)
-			}
-		}),
+		bot.WithEventListenerFunc(handler.onMessageCreate),
 	)
 	if err != nil {
 		slog.Error("failed to create client", "error", err)
